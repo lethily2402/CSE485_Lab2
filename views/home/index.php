22143c1bb3880f01491254d6f76119944729fd7f
tranthanh1401 <132728755+tranthanh1401@users.noreply.github.com>
2024-12-08 16:40:48 +0000

    index.php

---
+++ views/home/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Tin Tức Hàng Ngày</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
+</head>
+<body>
+   <section class="myheader">
+       <div class = "container py-3">
+           <div class="row">
+               <div class="col-md-3">
+                   <img src="image/logo.svg" alt="logo" class="img-fluid">
+               </div>
+               <div class="col-md-3">
+                   <div class="row">
+                       <div class="col-md-3">Chủ nhật, 8/12/2024</div>
+                       <div class="col-md-3">17 độ</div>
+                   </div>
+               </div>
+               <div class="col-md-3">
+                   <div class="row">
+                       <div class="col-md-3">Mới nhất</div>
+                       <div class="col-md-3">Tin theo khu vực</div>
+                   </div>
+               </div>
+               <div class="col-md-3">
+                   <div class="input-group mb-3">
+                       <input type="text" class="form-control" placeholder="Tìm kiếm" aria-label="Tìm kiếm" aria-describedby="basic-addon2">
+                       <span class="input-group-text" id="basic-addon2"><i class="bi bi-search"></i>    </span>
+                   </div>
+               </div>
+
+           </div>
+       </div>
+   </section>
+   <section class="mymainmenu">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-1 text-white">
+                   <i class="bi bi-house"></i>
+               </div>
+               <div class="col-md-10">
+                   <nav class="navbar navbar-expand-lg bg-danger">
+                       <div class="container-fluid">
+                           <a class="navbar-brand d-none" href="#">Navbar</a>
+                           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                               <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white active" aria-current="Thời sự" href="#"></a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white" href="#">Góc nhìn</a>
+                                   </li>
+                                   <li class="nav-item dropdown">
+                                       <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                           Thế giới
+                                       </a>
+                                       <ul class="dropdown-menu">
+                                           <li><a class="dropdown-item" href="#">Action</a></li>
+                                           <li><a class="dropdown-item" href="#">Another action</a></li>
+                                           <li><hr class="dropdown-divider"></li>
+                                           <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                       </ul>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Video</a>
+                                   </li>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Postcast</a>
+                                   </li></li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Kinh doanh</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Bất động sản</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Khoa học</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Giải trí</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Thể thao</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Pháp luật</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Giao dục</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Sức khoẻ</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link text-white">Đời sống</a>
+                                   </li>
+                               </ul>
+                           </div>
+                       </div>
+                   </nav>
+               </div>
+           </div>
+       </div>
+   </section>
+   <section class="mymaincontent">
+       <div class="container">
+           <div class="slider mb-3">
+               <div id="carouselExample" class="carousel slide">
+                   <div class="carousel-inner">
+                       <div class="carousel-item active">
+                           <img src="image/Slide.jpg" class="d-block w-100" alt="...">
+                       </div>
+                       <div class="carousel-item">
+                           <img src="image/Slide.jpg"" class="d-block w-100" alt="...">
+                       </div>
+                       <div class="carousel-item">
+                           <img src="image/Slide.jpg"" class="d-block w-100" alt="...">
+                       </div>
+                   </div>
+                   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                       <span class="visually-hidden">Previous</span>
+                   </button>
+                   <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                       <span class="visually-hidden">Next</span>
+                   </button>
+               </div>
+               <div class="slider">
+                   <div class="row">
+                       <div class="large-12 columns">
+                            <div class="class=owl-carousel-owl-theme">
+                                <div class="class = item">
+                                    <h4> 1</h4>
+                                </div>
+                                <div class="class = item">
+                                    <h4>2</h4>
+                                </div>
+                                <div class="class = item">
+                                    <h4>3</h4>
+                                </div>
+                                <div class="class = item">
+                                    <h4>4</h4>
+                                </div>
+                                <div class="class = item">
+                                    <h4>5</h4>
+                                </div>
+                            </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </section>
+   <section class="myfooter bg-dark text-white">
+        <div class="contaner">
+            <div class="row">
+                <div class="col-md-3">
+                    <ul class="list-menu">
+                        <li class="li_menu"><a href="/" tile="Trang chủ">Trang chủ</a></li>
+                        <li class="li_menu"><a href="/Video" tile="Video">Video</a></li>
+                        <li class="li_menu"><a href="/Postcards" tile="Postcards">Postcards</a></li>
+                        <li class="li_menu"><a href="/Anh" tile="Ảnh">Ảnh</a></li>
+                        <li class="li_menu"><a href="/Inphographics" tile="Inphographics">Inphographics</a></li>
+                        <li class="li_menu"><a href="/Moi-nhat" tile="Mới nhất">Mới nhất</a></li>
+                        <li class="li_menu"><a href="/Tin-nong" tile="Tin nóng">Tin nóng</a></li>
+                        <li class="li_menu"><a href="/Xem-nhieu" tile="Xem nhiều">Xem nhiều</a></li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="row"></div>
+            <div class="row"></div>
+            <div class="row"></div>
+
+        </div>
+   </section>
+
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
