4c6c7ff2295364f10afbeb752aa106b7705d70b6
tranthanh1401 <132728755+tranthanh1401@users.noreply.github.com>
2024-12-07 19:28:17 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge"
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Tin Tức Hàng Ngày</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
+</head>
+<body>
+   <section class="myheader">
+       <div class = "container py-3">
+           <div class="row">
+               <div class="col-md-3">
+                   <img src="image/logo.svg" alt="logo" class="img-fluid">
+               </div>
+               <div class="col-md-3">
+                   <div class="row">
+                       <div class="col-md-3">Chủ nhật, 8/12/2024</div>
+                       <div class="col-md-3">17 độ</div>
+                   </div>
+               </div>
+               <div class="col-md-3">
+                   <div class="row">
+                       <div class="col-md-3">Mới nhất</div>
+                       <div class="col-md-3">Tin theo khu vực</div>
+                   </div>
+               </div>
+               <div class="col-md-3">
+                   <div class="input-group mb-3">
+                       <input type="text" class="form-control" placeholder="Tìm kiếm" aria-label="Tìm kiếm" aria-describedby="basic-addon2">
+                       <span class="input-group-text" id="basic-addon2"><i class="bi bi-search"></i>    </span>
+                   </div>
+               </div>
+
+           </div>
+       </div>
+   </section>
+   <section class="myheader">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-1">
+
+               </div>
+               <div class="col-md-10">
+                   <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                       <div class="container-fluid">
+                           <a class="navbar-brand" href="#">Navbar</a>
+                           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                               <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                   <li class="nav-item">
+                                       <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link" href="#">Link</a>
+                                   </li>
+                                   <li class="nav-item dropdown">
+                                       <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                           Dropdown
+                                       </a>
+                                       <ul class="dropdown-menu">
+                                           <li><a class="dropdown-item" href="#">Action</a></li>
+                                           <li><a class="dropdown-item" href="#">Another action</a></li>
+                                           <li><hr class="dropdown-divider"></li>
+                                           <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                       </ul>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                                   </li>
+                               </ul>
+                           </div>
+                       </div>
+                   </nav>
+               </div>
+           </div>
+       </div>
+   </section>
+   <section class="myheader"></section>
+   <section class="myheader"></section>
+
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
